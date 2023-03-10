a1752ef2362c5eee9bc3a1bb96423b46a9ef5074
Ahmedgamalebied <93084790+Ahmedgamalebied@users.noreply.github.com>
2023-03-10 10:23:29 +0200

    Add files via upload

---
+++ lib/assets_manager.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AssetsManger {
+  static String imagepath = 'assets/images';
+  static String userimage = '$imagepath/person logo.png';
+  static String botimage = '$imagepath/bot.png';
+  static String openimage = '$imagepath/app_launcher.png';
+}
